f22531c223b033c43a8c07a72ec56bc0eb6c1683
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-01-09 15:52:13 -0300

    Add files via upload

---
+++ ImGui DirectX 9 Kiero Hook 2/includes.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "kiero/kiero.h"
+#include "kiero/minhook/include/MinHook.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_win32.h"
+#include "imgui/imgui_impl_dx9.h"
+#define WINDOW_NAME "Dear ImGui DirectX9 Example"
+typedef long(__stdcall* EndScene)(LPDIRECT3DDEVICE9);
+typedef LRESULT(CALLBACK* WNDPROC)(HWND, UINT, WPARAM, LPARAM);
---
+++ ImGui DirectX 9 Kiero Hook 2/main.cpp
@@ -0,0 +1,99 @@
+#include "includes.h"
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+EndScene oEndScene = NULL;
+WNDPROC oWndProc;
+static HWND window = NULL;
+
+void InitImGui(LPDIRECT3DDEVICE9 pDevice)
+{
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(pDevice);
+}
+
+bool init = false;
+long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
+{
+	if (!init)
+	{
+		InitImGui(pDevice);
+		init = true;
+	}
+
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("ImGui Window");
+	ImGui::End();
+
+	ImGui::EndFrame();
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+
+	return oEndScene(pDevice);
+}
+
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+		return true;
+
+	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+BOOL CALLBACK EnumWindowsCallback(HWND handle, LPARAM lParam)
+{
+	DWORD wndProcId;
+	GetWindowThreadProcessId(handle, &wndProcId);
+
+	if (GetCurrentProcessId() != wndProcId)
+		return TRUE; // skip to next window
+
+	window = handle;
+	return FALSE; // window found abort search
+}
+
+HWND GetProcessWindow()
+{
+	window = NULL;
+	EnumWindows(EnumWindowsCallback, NULL);
+	return window;
+}
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	bool attached = false;
+	do
+	{
+		if (kiero::init(kiero::RenderType::D3D9) == kiero::Status::Success)
+		{
+			kiero::bind(42, (void**)& oEndScene, hkEndScene);
+			do
+				window = GetProcessWindow();
+			while (window == NULL);
+			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWL_WNDPROC, (LONG_PTR)WndProc);
+			attached = true;
+		}
+	} while (!attached);
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		kiero::shutdown();
+		break;
+	}
+	return TRUE;
+}
