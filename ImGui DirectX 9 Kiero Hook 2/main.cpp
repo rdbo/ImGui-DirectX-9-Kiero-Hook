5fb7b5d8c1d4fff3392807e39af56da7fe02b117
Rdbo <57117082+rdbo@users.noreply.github.com>
2021-03-10 19:28:35 -0300

    Update main.cpp

--- ImGui DirectX 9 Kiero Hook 2/main.cpp
+++ ImGui DirectX 9 Kiero Hook 2/main.cpp
@@ -1,5 +1,9 @@
 #include "includes.h"
 
+#ifdef _WIN64
+#define GWL_WNDPROC GWLP_WNDPROC
+#endif
+
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 EndScene oEndScene = NULL;
